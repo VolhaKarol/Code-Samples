bd467fc035c7c80ec0046f574da816dd120a61bc
Volha Karol <shakuraolga@gmail.com>
2016-05-24 18:59:10 +0300

    Refactored code.

--- grab_kp.py
+++ grab_kp.py
@@ -2,38 +2,49 @@ from grab import Grab
 from urllib.parse import urljoin
 
 page_count = 3
-news_list = []
 start_url = 'http://www.kp.by'
+
+def execute_query(node, query):
+    nodes = node.select(query)
+    return nodes[0].text() if len(nodes) > 0 else None
+
 
 def process_news_page(grab, url):
     resp = grab.go(url)
-
     article_page = grab.doc.select("//div[@class='articlePage pagePad boxOver']")
-    if article_page:
-        ditem = dict.fromkeys(['author', 'title', 'time', 'comments'])
-        ditem['comments'] = article_page.select(".//div[@class='comments showCommentsJS']").text()
-        author = article_page.select(".//div[@class='authors']//a")
-        if len(author) > 0:
-            ditem['author'] = author.text()
-        ditem['title'] = article_page.select(".//h1").text()
-        news_list.append(ditem)
 
-def get_url(grab, page_num):
+    if article_page:
+        news_item = dict.fromkeys(['author', 'title', 'time', 'comments'])
+
+        news_item['comments'] = execute_query(article_page, ".//div[@class='comments showCommentsJS']")
+        news_item['author'] = execute_query(article_page, ".//div[@class='authors']//a")
+        news_item['title'] = execute_query(article_page, ".//h1")
+
+        return news_item
+
+    return None
+
+
+def get_urls(grab, page_num):
     page_url = 'http://www.kp.by/daily/minsk?pages.number=' + str(page_num)
     resp = grab.go(page_url)
 
     articles_links = [article.attr('href') for article in grab.doc.select("//article[@class='digest']//a[@href]")]
+    unique_articles = (article for article in set(articles_links) if article.startswith('/daily'))
 
-    for article in set(articles_links):
-        href = article
+    for article in unique_articles:
+        url = urljoin(start_url, article)
+        print(url)
+        yield url
 
-        if href.startswith('/daily'):
-            url = urljoin(start_url, href)
-            print(url)
-            process_news_page(grab, url)
-
+news_list = []
 g = Grab()
-for i in reversed(range(1,page_count + 1)):
-    get_url(g, i)
+
+for i in reversed(range(1, page_count + 1)):
+    urls = get_urls(g, i)
+    for url in urls:
+        news_item = process_news_page(g, url)
+        if news_item:
+            news_list.append(news_item)
 
 print(news_list)
