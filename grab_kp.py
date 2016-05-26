c1e3b3b2c8263b71fd0e4d3137f7ed1c34b64d80
Volha Karol <shakuraolga@gmail.com>
2016-05-26 17:30:41 +0300

    Expanded the list of information that is extracted from the website; implemented merge text of several nodes from query result.

--- grab_kp.py
+++ grab_kp.py
@@ -5,22 +5,35 @@ import pickle
 page_count = 3
 start_url = 'http://www.kp.by'
 
-def execute_query(node, query):
+
+def execute_query(node, query, first_only=True):
     nodes = node.select(query)
-    return nodes[0].text().strip() if len(nodes) > 0 else None
+
+    q_result = None
+
+    if len(nodes) > 0:
+        q_result = nodes[0].text().strip() if first_only else ' '.join(n.text().strip() for n in nodes)
+
+    return q_result
 
 
-def process_news_page(grab, url):
-    resp = grab.go(url)
-    article_page = grab.doc.select("//div[@class='articlePage pagePad boxOver']")
+def process_news_page(gra, page_url):
+    resp = grab.go(page_url)
+    article_page = grab.doc.select("//div[contains(@class, 'articlePage')]")
 
     if article_page:
-        news_item = dict.fromkeys(['author', 'title', 'time', 'comments'])
+        news_item = dict.fromkeys(['author', 'date', 'title', 'description', 'content', 'comments'])
 
-        news_item['comments'] = execute_query(article_page, ".//div[@class='comments showCommentsJS']")
-        news_item['author'] = execute_query(article_page, ".//div[@class='authors']//a")
+        news_item['author'] = execute_query(article_page, ".//div[contains(@class, 'authors')]//a")
+        news_item['comments'] = execute_query(article_page, ".//div[contains(@class, 'comments')]") or '0'
         news_item['title'] = execute_query(article_page, ".//h1")
+        news_item['description'] = execute_query(article_page, ".//*[not(contains(@class, 'video'))]//h2")
+        news_item['content'] = execute_query(
+            article_page,
+            ".//div[contains(@class, 'text')]/*[not(contains(@class, 'coverImg')) and not(contains(@class, 'video'))]",
+            False)
 
+        news_item['date'] = execute_query(article_page, ".//time")
         return news_item
 
     return None
@@ -50,8 +63,5 @@ for i in reversed(range(1, page_count + 1)):
         if news_item:
             news_list.append(news_item)
 
-
 with open('news_list.pickle', 'wb') as f:
     pickle.dump(news_list, f)
-
-print(news_list)
